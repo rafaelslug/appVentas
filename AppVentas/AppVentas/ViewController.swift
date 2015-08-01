a699aea5b20052684191887e20b249a2a4170ca4
Lorenzo Rafael Ortiz Diaz <rafaelslug_20@hotmail.com>
2015-08-01 11:32:10 -0500

    Creando proyecto appVentas

---
+++ AppVentas/AppVentas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppVentas
+//
+//  Created by Lorenzo on 01/08/15.
+//  Copyright (c) 2015 Lorenzo Rafael Ortiz Diaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
